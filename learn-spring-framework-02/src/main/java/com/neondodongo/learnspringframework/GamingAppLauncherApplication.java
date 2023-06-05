64071de93122dfffe5a448503ec5e61124357aaa
neondodongo <willirmler@me.com>
2023-06-05 16:16:43 -0500

    components and component scan!

---
+++ learn-spring-framework-02/src/main/java/com/neondodongo/learnspringframework/GamingAppLauncherApplication.java
@@ -0,0 +1,20 @@
+package com.neondodongo.learnspringframework;
+
+import com.neondodongo.learnspringframework.game.*;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+// Instead of a separate Configuration class, we can give the main class the annotation
+@Configuration
+// We have to tell Spring where to find the beans via the package name
+@ComponentScan("com.neondodongo.learnspringframework.game")
+public class GamingAppLauncherApplication {
+
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class);
+
+        context.getBean(GamingConsole.class).up();
+        context.getBean(GameRunner.class).run();
+    }
+}
---
+++ learn-spring-framework-02/src/main/java/com/neondodongo/learnspringframework/game/PacmanGame.java
@@ -0,0 +1,23 @@
+package com.neondodongo.learnspringframework.game;
+
+import org.springframework.stereotype.Component;
+
+// Component indicates to Spring that this class should be managed
+@Component
+public class PacmanGame implements GamingConsole{
+    public void up() {
+        System.out.println("waka up");
+    }
+
+    public void down() {
+        System.out.println("waka down");
+    }
+
+    public void left() {
+        System.out.println("waka left");
+    }
+
+    public void right() {
+        System.out.println("waka right");
+    }
+}
